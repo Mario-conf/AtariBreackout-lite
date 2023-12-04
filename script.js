e7c612d1ec213adb1d1d696d38d13b73748cbe6a
Mario AV <152533010+Mario-conf@users.noreply.github.com>
2023-12-04 21:12:55 +0100

    Update v14'7 script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+window.onload = function () {
     const canvas = document.getElementById("breakoutCanvas");
     const ctx = canvas.getContext("2d");
 
@@ -80,51 +80,55 @@ window.addEventListener("load", function () {
         ctx.closePath();
     }
 
-function drawBricks() {
-    for (let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
-            let brickX = c * (brickWidth + brickPadding) + (canvas.width - (brickColumnCount * (brickWidth + brickPadding))) / 2;
-            let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-            if (bricks[c][r] === undefined) {
-                bricks[c][r] = { x: brickX, y: brickY, status: 1, color: getRandomColor() };
-            } else {
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
+    function drawBricks() {
+        for (let c = 0; c < brickColumnCount; c++) {
+            bricks[c] = [];
+            for (let r = 0; r < brickRowCount; r++) {
+                if (bricks[c][r] === undefined || bricks[c][r].status === 1) {
+                    let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                    let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                    if (bricks[c][r] === undefined) {
+                        bricks[c][r] = { x: brickX, y: brickY, status: 1, color: getRandomColor() };
+                    } else {
+                        bricks[c][r].x = brickX;
+                        bricks[c][r].y = brickY;
+                    }
+                    ctx.beginPath();
+                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                    ctx.fillStyle = bricks[c][r].color;
+                    ctx.fill();
+                    ctx.closePath();
+                }
             }
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = bricks[c][r].color;
-            ctx.fill();
-            ctx.closePath();
         }
     }
-}
 
-function collisionDetection() {
-    for (let c = 0; c < brickColumnCount; c++) {
-        for (let r = 0; r < brickRowCount; r++) {
-            let b = bricks[c][r];
-            if (b.status === 1) {
-                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-                    dy = -dy;
-                    b.status = 0;
-                    score++;
+    function collisionDetection() {
+        for (let c = 0; c < brickColumnCount; c++) {
+            for (let r = 0; r < brickRowCount; r++) {
+                let b = bricks[c][r];
+                if (b.status === 1) {
+                    if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                        dy = -dy;
+                        b.status = 0;
+                        score++;
 
-                    if (score === brickRowCount * brickColumnCount) {
-                        level++;
-                        score = 0;
-                        brickRowCount++; // Aumentar filas en cada nuevo nivel
-                        brickColumnCount++; // Aumentar columnas en cada nuevo nivel
-                        resetBricks();
-                        // Aumentar velocidad del balón en cada nuevo nivel
-                        dx += 0.5;
-                        dy += 0.5;
+                        if (score === brickRowCount * brickColumnCount) {
+                            level++;
+                            score = 0;
+                            brickRowCount++; // Aumentar filas en cada nuevo nivel
+                            brickColumnCount++; // Aumentar columnas en cada nuevo nivel
+                            resetBricks();
+                            // Aumentar velocidad del balón en cada nuevo nivel
+                            dx = (dx > 0) ? dx + 1 : dx - 1;
+                            dy = (dy > 0) ? dy + 1 : dy - 1;
+                        }
                     }
                 }
             }
         }
     }
-}
+
     function resetBricks() {
         for (let c = 0; c < brickColumnCount; c++) {
             for (let r = 0; r < brickRowCount; r++) {
